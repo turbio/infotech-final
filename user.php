d9d01da1ed579ce95da4f15fcc50017bf74e991c
turb <turb@pisscake.com>
2015-05-25 00:09:47 -0700

    more unimplemented functions

--- user.php
+++ user.php
@@ -21,16 +21,32 @@ class user{
 		throw new Exception('no session');
 	}
 
+	//checks if user has the correct credentials, then set the user's
+	//evironment variables if they do
 	static function login($name, $password, $database){
 		//TODO implement this
 		throw new Exception('not implemented');
 	}
 
+	//destroy all session information thus logging the user out
+	static function logout(){
+		//TODO implement this
+		throw new Exception('not implemented');
+	}
+
+	//create an entry in the user's table for the desired username
 	static function signup($name, $password, $database){
 		//TODO implement this
 		throw new Exception('not implemented');
 	}
 
+	//check if a user exists with the given username
+	static function userExists($name, $database){
+		//TODO implement this
+		throw new Exception('not implemented');
+	}
+
+	//makes sure username is valo ie alphanumeric
 	static function validateUsername($name){
 		//TODO implement this
 		throw new Exception('not implemented');
