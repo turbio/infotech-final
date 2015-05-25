f437e170c9ae493ad2db7bc6cb6d1c996b7437ab
Tony Reks <toscarreks@gmail.com>
2015-05-24 23:29:58 -0700

    Organizing

--- register.php
+++ register.php
@@ -29,7 +29,7 @@ function verifyNick($nick){
 
 	return true;
 }
-
+//If the user registers, and the name/nick is valid, it will add a new user at $userAddQuery
 if(!isset($_SESSION['user'])){
 	if(!empty($_POST["nick"]) && !empty($_POST["password"])){
 		$nickValidity = verifyNick($_POST["nick"]);
@@ -47,8 +47,12 @@ if(!isset($_SESSION['user'])){
 		}else{
 			$template->error = true;
 		}
+		//here there is an implicit $template-error = false, because its been assigned that by default
+		// I feel there might be an exception there, or some loophole. Might investigate later
 	}
 }else{
+	//Why are register and loggedIn separate variables? 
+	//They both execute the same things in the template. 
 	$template->loggedIn = true;
 }
 
