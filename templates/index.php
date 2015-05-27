b82dd921f0a021fdb4135526f503d8ddcb962d84
turb <turb@pisscake.com>
2015-05-27 15:42:22 -0700

    added splash page with resource folder, all images should be put here

--- templates/index.php
+++ templates/index.php
@@ -1,14 +1,29 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="splash.css">
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
 </head>
 <body>
 
-<?php
-
-echo 'splash placeholder';
-
-?>
+<div id="splashbar">
+	<div id="logo_container" class="splashbar_button">
+		<div id="logo_icon_container">
+			<object type="image/svg+xml" data="res/logo.svg" id="logo">
+				Kiwi Logo
+			</object>
+		</div>
+		<div id="logo_text">Title</div>
+	</div>
+		<div id="right_button_container">
+		<div class="splashbar_button" id="sign_up_button">sign up</div>
+		<div class="splashbar_button">sign in</div>
+		<div class="splashbar_button">TODO</div>
+	</div>
+</div>
+<div class="image_slide">
+	<img src="res/splash_photo.jpg"/>
+</div>
 
 </body>
 </html>
