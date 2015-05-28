fa31620f554e20f99e3cc291db8a95cf5c7dc4e9
turb <turb@pisscake.com>
2015-05-28 14:11:32 -0700

    added captcha, not yet implemented on the server side though

--- templates/signup.php
+++ templates/signup.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="style.css">
+<script src='https://www.google.com/recaptcha/api.js'></script>
 <?php
 echo '<div class="dialog">';
 echo '<div class="dialog_title">sign up</div>';
@@ -10,7 +11,11 @@ echo '<div id="fail_box">invalid username</div>';
 echo '<form action="signup.php" method="POST">
 		<input type="text" name="username" placeholder="username" class="field">
 		<br/>
+		<input type="text" name="email" placeholder="email" class="field">
+		<br/>
 		<input type="password" name="password" placeholder="password" class="field">
+		<br/>
+		<div class="g-recaptcha" id="captcha" data-sitekey="6LcPjwcTAAAAAP_cseFYvlfIpXSf31AmKjdwdS4q"></div>
 		<br/>
 		<input type="submit" value="sign up" class="button">
 	</form>';
