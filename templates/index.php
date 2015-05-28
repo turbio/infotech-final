ce0a27fcd7c7aa1478194852c367df50a19056e7
turb <turb@pisscake.com>
2015-05-27 21:48:13 -0700

    more footer stuff

--- templates/index.php
+++ templates/index.php
@@ -5,40 +5,41 @@
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
 </head>
 <body>
-
-<div class="image_slide">
-	<img src="res/splash_photo.jpg"/>
-</div>
-<div id="splashbar">
-	<div id="logo_container" class="splashbar_button">
-		<div id="logo_icon_container">
-			<object type="image/svg+xml" data="res/logo.svg" id="logo">
-				logo
-			</object>
+<div id="wrapper">
+	<div class="image_slide">
+		<img src="res/splash_photo.jpg"/>
+	</div>
+	<div id="splashbar">
+		<div id="logo_container" class="splashbar_button">
+			<div id="logo_icon_container">
+				<object type="image/svg+xml" data="res/logo.svg" id="logo">
+					logo
+				</object>
+			</div>
+			<div id="logo_text">Title</div>
 		</div>
-		<div id="logo_text">Title</div>
-	</div>
-		<div id="right_button_container">
-		<div class="splashbar_button" id="sign_up_button">sign up</div>
-		<div class="splashbar_button">sign in</div>
-		<div class="splashbar_button">
-			<object type="image/svg+xml" data="res/steam.svg" id="steam_logo">
-				steam logo
-			</object>
+			<div id="right_button_container">
+			<div class="splashbar_button" id="sign_up_button">sign up</div>
+			<div class="splashbar_button">sign in</div>
+			<div class="splashbar_button">
+				<object type="image/svg+xml" data="res/steam.svg" id="steam_logo">
+					steam logo
+				</object>
+			</div>
 		</div>
 	</div>
-</div>
-<div id="content_container">
-	<div id="message">
-<?php
-echo "<h1>community of 0+ users</h1>";
-echo "<br/>";
-echo "Lorem ipsum dolor sit amet, vis latine veritus perfecto eu, has cu tollit adolescens interesset. No sit fierent definiebas, no nisl case accumsan quo. Ad vis nominavi qualisque dignissim, ei vide tacimates percipitur sea, no sea officiis eleifend. Ea pro mnesarchum voluptatum. Est errem possit verear ut. Mea ut mutat expetenda.";
-?>
-		<div class="splashbar_button" id="sign_up_button">sign up</div>
+	<div id="content_container">
+		<div id="message">
+	<?php
+	echo "<h1>community of 0+ users</h1>";
+	echo "<br/>";
+	echo "Lorem ipsum dolor sit amet, vis latine veritus perfecto eu, has cu tollit adolescens interesset. No sit fierent definiebas, no nisl case accumsan quo. Ad vis nominavi qualisque dignissim, ei vide tacimates percipitur sea, no sea officiis eleifend. Ea pro mnesarchum voluptatum. Est errem possit verear ut. Mea ut mutat expetenda.";
+	?>
+			<div class="splashbar_button" id="sign_up_button">sign up</div>
+		</div>
 	</div>
-</div>
 
-<div id="footer">footer stuff here</div>
+	<div id="footer"><div class="filler">footer stuff here</div></div>
+</div>
 </body>
 </html>
