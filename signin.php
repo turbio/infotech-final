9af0272c7d2e1c4c3161a41f4542a8250775e992
turb <turb@pisscake.com>
2015-06-04 16:36:56 -0700

    removed conflict in signin

--- signin.php
+++ signin.php
@@ -24,34 +24,5 @@ if(!empty($_POST['username'])
 		$template->error = $e->getMessage();
 	}
 }
-echo "test";
-if(!isset($_SESSION['user']) || !isset($_SESSION['id'])){
-	if(!empty($_POST["username"]) && !empty($_POST["password"])){
-		$template->loginAttempt = true;
-
-		$userQuery = $database->query(
-			'SELECT * FROM users WHERE username = "'. $_POST["username"] . '";');
-		$userInfo = $userQuery->fetch();
-
-		if(!empty($userInfo["password"]) && !empty($userInfo["salt"]) && !empty($userInfo["id"])){
-			$rehashedPass = hash("sha256", $_POST["password"] . $userInfo["salt"]);
-
-			if($userInfo["password"] == $rehashedPass){
-				$template->correctCred = true;
-				$template->loggedIn = true;
-				$_SESSION['user'] = $userInfo["username"];
-				$_SESSION['id'] = $userInfo["id"];
-				echo "IT PASSED THROUGH";
-			}
-
-		}else{
-			$template->correctCred = false;
-			echo "INCORRECT CRED";
-		}
-	}
-}else{
-	$template->loggedIn = true;
-	echo "LOGGED IN";
-}
 
 $template->render('signin.php');
